eae41ffb97f627e6d03d425a64f7a9b4768bf0c6
David Muchahi <muchahi853@gmail.com>
2025-04-14 18:15:47 +0300

    Finished lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    
+    even_number=[n for n in num_list if n%2==0]
+    return even_number
+
+print(return_evens(range(20)))
+    
+
+
+  
+
 
 def make_exclamation(sentence_list):
-    pass
+    return[sentence+"!" for sentence in sentence_list]
+
